92588afa69010cc3b9e565e40e0200123f871df7
Jacek Jusianiec <jjusianiec@gmail.com>
2019-06-21 15:21:20 +0200

    Create descriptor.js

---
+++ descriptor.js
@@ -0,0 +1,37 @@
+define([
+    './sidebar/sidebar.module',
+    './commons/commons.module',
+    './timesheets/timesheets.module',
+    './my-projects/my-projects.module',
+    './my-skills/my-skills.module',
+    './apo-resourcer/apo-resourcer.module',
+    './free-time/free-time.module',
+    './team-leader/team-leader.module',
+    './hr-manager/hr-manager.module',
+    './bookkeeper/bookkeeper.module',
+    './projects/projects.module'
+
+], function () {
+    
+    var app = angular.module('apsello',
+        ['i18n.translateProvider',
+            'ngRoute',
+            // ...
+            "sidebar",
+            "timesheets",
+            "freeTime",
+            'myProjects',
+            'mySkills',
+            'projects',
+            'apoResourcer',
+            "teamLeader",
+            "hrManager",
+            'bookkeeper'
+        ]);
+
+    // ...
+
+    angular.bootstrap(document.getElementsByTagName("html")[0], ["apsello"]);
+
+    return app;
+});
